625f48d48c6d23e84c7d006419dd9662483492c9
lucienyida <96853173+lucienyida@users.noreply.github.com>
2025-05-09 12:26:14 +0800

    Update const.py

--- custom_components/water_meter/const.py
+++ custom_components/water_meter/const.py
@@ -1,3 +1,14 @@
-# custom_components/water_meter/const.py
-"""Constants for water meter integration."""
 DOMAIN = "water_meter"
+
+SENSOR_KEYS_ORDER = [
+    "owner_no",
+    "balance",
+    "current_usage",
+    "current_bill",
+    "current_year_total_usage",
+    "annual_bill",
+    "last_year_total_usage",
+    "monthly_usage",
+    "latest_reading_time",
+    "latest_reading_value"
+]
